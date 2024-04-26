b1ddaf4cc6fb2dbd2f6f03df05fa6040541aab99
JeremyBarrios <166885842+JeremyBarrios@users.noreply.github.com>
2024-04-25 19:21:53 -0600

    triangulo, circulo y cuadrado agregados

--- Proyecto Final/main.cpp
+++ Proyecto Final/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
+#include <cmath>
 
 using namespace std;
 
@@ -10,7 +11,6 @@ const int SCREEN_HEIGHT = 20;
 char screen[SCREEN_HEIGHT][SCREEN_WIDTH];
 int cursorX = SCREEN_WIDTH / 2;
 int cursorY = SCREEN_HEIGHT / 2;
-
 
 void drawScreen() {
     system("cls");
@@ -22,23 +22,16 @@ void drawScreen() {
     }
 }
 
-
 void moveCursor(int dx, int dy) {
-
     int newX = cursorX + dx;
     int newY = cursorY + dy;
-
-
     if (newX < 0) newX = SCREEN_WIDTH - 1;
     if (newX >= SCREEN_WIDTH) newX = 0;
     if (newY < 0) newY = SCREEN_HEIGHT - 1;
     if (newY >= SCREEN_HEIGHT) newY = 0;
-
-
     cursorX = newX;
     cursorY = newY;
 }
-
 
 class Shape {
 public:
@@ -46,7 +39,6 @@ public:
     virtual void erase() = 0;
 };
 
-// Clase para el Cuadrado
 class Square : public Shape {
 public:
     void draw() override {
@@ -72,9 +64,55 @@ public:
     }
 };
 
+class Circle : public Shape {
+public:
+    void draw() override {
+        int radius = 3;
+        int centerX = cursorX + 2;
+        int centerY = cursorY + 2;
+
+        for (int y = cursorY; y < cursorY + 5; y++) {
+            for (int x = cursorX; x < cursorX + 5; x++) {
+                double distance = sqrt(pow(x - centerX, 2) + pow(y - centerY, 2));
+                if (distance <= radius) {
+                    screen[y % SCREEN_HEIGHT][x % SCREEN_WIDTH] = '+';
+                }
+            }
+        }
+    }
+
+    void erase() override {
+        for (int y = cursorY; y < cursorY + 5; y++) {
+            for (int x = cursorX; x < cursorX + 5; x++) {
+                screen[y % SCREEN_HEIGHT][x % SCREEN_WIDTH] = ' ';
+            }
+        }
+    }
+};
+
+class Triangle : public Shape {
+public:
+    void draw() override {
+        screen[cursorY][cursorX + 2] = '+';
+        screen[cursorY + 1][cursorX + 1] = '+';
+        screen[cursorY + 1][cursorX + 3] = '+';
+        screen[cursorY + 2][cursorX] = '+';
+        screen[cursorY + 2][cursorX + 1] = '+';
+        screen[cursorY + 2][cursorX + 2] = '+';
+        screen[cursorY + 2][cursorX + 3] = '+';
+        screen[cursorY + 2][cursorX + 4] = '+';
+    }
+
+    void erase() override {
+        for (int y = cursorY; y < cursorY + 3; y++) {
+            for (int x = cursorX; x < cursorX + 5; x++) {
+                screen[y % SCREEN_HEIGHT][x % SCREEN_WIDTH] = ' ';
+            }
+        }
+    }
+};
 
 int main() {
-
     for (int y = 0; y < SCREEN_HEIGHT; y++) {
         for (int x = 0; x < SCREEN_WIDTH; x++) {
             screen[y][x] = ' ';
@@ -82,43 +120,53 @@ int main() {
     }
 
     Square square;
+    Circle circle;
+    Triangle triangle;
 
     while (true) {
+        if (_kbhit()) {
+            char key = _getch();
+            switch (key) {
+                case 'w':
+                    moveCursor(0, -1);
+                    break;
+                case 's':
+                    moveCursor(0, 1);
+                    break;
+                case 'a':
+                    moveCursor(-1, 0);
+                    break;
+                case 'd':
+                    moveCursor(1, 0);
+                    break;
+                case 'f':
+                    square.draw();
+                    break;
+                case 'e':
+                    square.erase();
+                    break;
+                case 'c':
+                    circle.draw();
+                    break;
+                case 'r':
+                    circle.erase();
+                    break;
+                case 't':
+                    triangle.draw();
+                    break;
+                case 'g':
+                    triangle.erase();
+                    break;
+                case 'q':
+                    return 0;
+            }
+        }
 
         screen[cursorY][cursorX] = '*';
 
-
         drawScreen();
 
-
         screen[cursorY][cursorX] = ' ';
-
-
-        char key = _getch();
-
-
-        switch (key) {
-            case 'w':
-                moveCursor(0, -1);
-                break;
-            case 's':
-                moveCursor(0, 1);
-                break;
-            case 'a':
-                moveCursor(-1, 0);
-                break;
-            case 'd':
-                moveCursor(1, 0);
-                break;
-            case 'f': // Tecla 'F' para dibujar un cuadrado
-                square.draw();
-                break;
-            case 'e': // Tecla 'E' para borrar el cuadrado
-                square.erase();
-                break;
-            case 'q': // Tecla 'Q' para salir del programa
-                return 0;
-        }
     }
 
     return 0;
